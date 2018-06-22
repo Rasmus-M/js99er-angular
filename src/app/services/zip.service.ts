8003a063f0b85079553567235a4af6b9141f9c45
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-06-22 18:15:07 +0200

    Zip

--- src/app/services/zip.service.ts
+++ src/app/services/zip.service.ts
@@ -1,5 +1,56 @@
-import {Injectable, OnInit} from '@angular/core';
-import {ModuleService} from './module.service';
+import {Injectable} from '@angular/core';
+
+// Modification of zip.js HttpReader that uses GET instead of HEAD
+class HttpReader {
+
+    url: string;
+    size = 0;
+    data: Uint8Array;
+
+    constructor(url) {
+        this.url = url;
+    }
+
+    getData(callback, onerror) {
+        const that = this;
+        let request;
+        if (!this.data) {
+            request = new XMLHttpRequest();
+            request.addEventListener("load", function() {
+                if (!that.size) {
+                    that.size = Number(request.getResponseHeader("Content-Length"));
+                }
+                that.data = new Uint8Array(request.response);
+                callback();
+            }, false);
+            request.addEventListener("error", onerror, false);
+            request.open("GET", this.url);
+            request.responseType = "arraybuffer";
+            request.send();
+        } else {
+            callback();
+        }
+    }
+
+    init(callback, onerror) {
+        const that = this;
+        const request = new XMLHttpRequest();
+        request.addEventListener("load", function() {
+            that.size = Number(request.getResponseHeader("Content-Length"));
+            callback();
+        }, false);
+        request.addEventListener("error", onerror, false);
+        request.open("GET", this.url); // Changed from HEAD request
+        request.send();
+    }
+
+    readUint8Array(index, length, callback, onerror) {
+        const that = this;
+        this.getData(function() {
+            callback(new Uint8Array(that.data.subarray(index, index + length)));
+        }, onerror);
+    }
+}
 
 @Injectable()
 export class ZipService {
@@ -15,7 +66,7 @@ export class ZipService {
     }
 
     createHttpReader(url: string) {
-        return new this.zip.HttpReader(url);
+        return new HttpReader(url);
     }
 
     createReader(reader: any, callback: (zipReader) => void, onerror: (err: any) => void) {
