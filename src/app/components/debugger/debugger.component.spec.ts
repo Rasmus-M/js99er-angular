de8461bce490855e256959df00032618a055946d
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-07-28 11:59:01 +0200

    Optimize imports

--- src/app/components/debugger/debugger.component.spec.ts
+++ src/app/components/debugger/debugger.component.spec.ts
@@ -2,13 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DebuggerComponent} from './debugger.component';
 import {NO_ERRORS_SCHEMA} from "@angular/core";
-import {ActivatedRoute, Router} from "@angular/router";
-import {AudioService} from "../../services/audio.service";
-import {SettingsService} from "../../services/settings.service";
-import {ModuleService} from "../../services/module.service";
-import {HttpClient} from "@angular/common/http";
 import {CommandDispatcherService} from "../../services/command-dispatcher.service";
-import {DiskService} from "../../services/disk.service";
 
 describe('DebuggerComponent', () => {
     let component: DebuggerComponent;
