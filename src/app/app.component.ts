fc56dab18ba50be4fd9be6b2bc465c29b7b7447b
Rasmus-M <rasmus.moustgaard@gmail.com>
2019-01-10 21:45:18 +0100

    Version 7.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,8 +33,8 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
     tabIndex: number;
 
     title = "JS99'er";
-    version = "7.4.7";
-    date = "6 January, 2019";
+    version = "7.5";
+    date = "10 January, 2019";
 
     private routerSubscription: Subscription;
     private commandSubscription: Subscription;
