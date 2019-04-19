7c05d35226815470b3b39a674df2d7271a76cea3
Rasmus-M <rasmus.moustgaard@gmail.com>
2019-04-19 20:42:07 +0200

    Routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,6 @@
 import {async, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {AudioService} from "./services/audio.service";
-import {CommandDispatcherService} from "./services/command-dispatcher.service";
-import {SettingsService} from "./services/settings.service";
-import {DiskService} from "./services/disk.service";
-import {ModuleService} from "./services/module.service";
-import {HttpClient} from "@angular/common/http";
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
