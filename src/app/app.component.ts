d9d02d93861ec3a4ca2397f1576f10ff3487e38c
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2020-06-15 20:59:44 +0200

    Fix split view for VDP memory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import {Component} from "@angular/core";
 export class AppComponent {
 
     static readonly TITLE = "JS99'er";
-    static readonly VERSION = "7.9.0";
-    static readonly DATE = "14 May, 2020";
+    static readonly VERSION = "7.9.1";
+    static readonly DATE = "15 June, 2020";
 }
