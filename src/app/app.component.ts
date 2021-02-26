1e5f3c629b63df180dd7859eba8e7b066488d21b
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2021-02-26 18:15:37 +0100

    Version 8.4.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import {Component} from "@angular/core";
 export class AppComponent {
 
     static readonly TITLE = "JS99'er";
-    static readonly VERSION = "8.4.0";
-    static readonly DATE = "13 February, 2021";
+    static readonly VERSION = "8.4.1";
+    static readonly DATE = "February 26, 2021";
 }
