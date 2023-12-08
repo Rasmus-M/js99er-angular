29325c0f091919f499d7455103a510193990af66
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-12-08 18:06:29 +0100

    Service worker; Ghostbusters

--- src/app/js99er.component.ts
+++ src/app/js99er.component.ts
@@ -20,8 +20,8 @@ export const customTooltipDefaults: MatTooltipDefaultOptions = {
 export class Js99erComponent implements OnInit {
 
     static readonly TITLE = "JS99'er";
-    static readonly VERSION = "9.5.1";
-    static readonly DATE = "December 3, 2023";
+    static readonly VERSION = "9.5.3";
+    static readonly DATE = "December 5, 2023";
 
     constructor(
         private elm: ElementRef,
