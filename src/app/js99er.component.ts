9013d7754cc5bb67fc70a1d31acbd0e7fb7da17f
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-08-17 21:00:06 +0200

    Update gbust and fbforth

--- src/app/js99er.component.ts
+++ src/app/js99er.component.ts
@@ -19,8 +19,8 @@ export const customTooltipDefaults: MatTooltipDefaultOptions = {
 export class Js99erComponent implements OnInit {
 
     static readonly TITLE = "JS99'er";
-    static readonly VERSION = "10.3.6";
-    static readonly DATE = "August 10, 2024";
+    static readonly VERSION = "10.3.7";
+    static readonly DATE = "August 17, 2024";
 
     constructor(
         private elm: ElementRef,
