8190f0a399757f0f04b396d357b4d921f8c56d24
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-09-06 19:15:17 +0200

    Add Stealth Runner and Dragon's Lair demo since speech is working now

--- src/app/js99er.component.ts
+++ src/app/js99er.component.ts
@@ -19,8 +19,8 @@ export const customTooltipDefaults: MatTooltipDefaultOptions = {
 export class Js99erComponent implements OnInit {
 
     static readonly TITLE = "JS99'er";
-    static readonly VERSION = "10.3.9";
-    static readonly DATE = "September 2, 2024";
+    static readonly VERSION = "10.4.0";
+    static readonly DATE = "September 6, 2024";
 
     constructor(
         private elm: ElementRef,
