f9c6ddd309d9508beb274a67789d7689cb45be51
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-10-16 18:38:51 +0200

    Enable proxy to support one redirect.

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -5,29 +5,37 @@ const fs = require('fs');
 
 const myConsole = new console.Console(fs.createWriteStream('./log.txt'));
 
+function getFile(queryUrl, callback) {
+    if (queryUrl.startsWith('https')) {
+        https.get(queryUrl, callback);
+    } else {
+        http.get(queryUrl, callback);
+    }
+}
+
+function downloadFile(queryUrl, response, res)  {
+    res.writeHead(200, {
+        'Content-Type': 'application/octet-stream',
+        'Content-disposition': 'attachment; filename=' + queryUrl.split('/').pop()
+    });
+    response.pipe(res);
+}
+
 const server = http.createServer((req, res) => {
     const query = url.parse(req.url, true).query;
     const queryUrl = query['url'];
     if (queryUrl) {
         myConsole.log("Downloading " + queryUrl + "\n");
         try {
-            if (queryUrl.startsWith('https')) {
-                https.get(queryUrl, (response) => {
-                    res.writeHead(200, {
-                        'Content-Type': 'application/octet-stream',
-                        'Content-disposition': 'attachment; filename=' + queryUrl.split('/').pop()
-                    });
-                    response.pipe(res);
-                });
-            } else {
-                http.get(queryUrl, (response) => {
-                    res.writeHead(200, {
-                        'Content-Type': 'application/octet-stream',
-                        'Content-disposition': 'attachment; filename=' + queryUrl.split('/').pop()
-                    });
-                    response.pipe(res);
-                });
-            }
+            getFile(queryUrl, (response) => {
+                if (response.headers.location) {
+                    getFile(response.headers.location, (response2) => {
+                        downloadFile(queryUrl, response2, res);
+                    })
+                } else {
+                    downloadFile(queryUrl, response, res);
+                }
+            });
         } catch (err) {
             myConsole.error(err);
             res.writeHead(400, {'Content-Type': 'text/plain'});
