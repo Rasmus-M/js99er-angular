573203ff60e720f690db3bf56ee03b559cba6f84
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-11-09 20:01:13 +0100

    Version 12.6.0

--- src/app/js99er.component.ts
+++ src/app/js99er.component.ts
@@ -19,8 +19,8 @@ export const customTooltipDefaults: MatTooltipDefaultOptions = {
 export class Js99erComponent implements OnInit {
 
     static readonly TITLE = "JS99'er";
-    static readonly VERSION = "12.5.2";
-    static readonly DATE = "November 07, 2024";
+    static readonly VERSION = "12.6.0";
+    static readonly DATE = "November 09, 2024";
 
     constructor(
         private elm: ElementRef,
