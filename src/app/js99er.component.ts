91cc570ef9c5767d32de08f3098e06dddf0fdb92
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2024-12-22 09:54:59 +0100

    Option width

--- src/app/js99er.component.ts
+++ src/app/js99er.component.ts
@@ -19,7 +19,7 @@ export const customTooltipDefaults: MatTooltipDefaultOptions = {
 export class Js99erComponent implements OnInit {
 
     static readonly TITLE = "JS99'er";
-    static readonly VERSION = "15.0.0";
+    static readonly VERSION = "15.0.1";
     static readonly DATE = "December 22, 2024";
 
     constructor(
