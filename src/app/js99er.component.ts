a19d3efe3a3e2971a2602cc0f116e9924cdb83df
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-11-15 14:43:58 +0100

    Update TIPI DSR

--- src/app/js99er.component.ts
+++ src/app/js99er.component.ts
@@ -19,8 +19,8 @@ export const customTooltipDefaults: MatTooltipDefaultOptions = {
 export class Js99erComponent implements OnInit {
 
     static readonly TITLE = "JS99'er";
-    static readonly VERSION = "15.2.2";
-    static readonly DATE = "November 13, 2025";
+    static readonly VERSION = "15.3.0";
+    static readonly DATE = "November 15, 2025";
 
     constructor(
         private elm: ElementRef,
